6f820cef150baa18fef53d9d7423a5dadf86a571
Javier Vieiro <jmvieiro@gmail.com>
2021-06-03 00:02:27 -0300

    Store JMV

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos a Store JMV
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ingresar
         </a>
       </header>
     </div>
